659bf54cb1186c38333ec290b4e170949999fd0a
Patrick Wüller <patrick_wueller@gmx.de>
2015-05-09 17:05:49 +0200

    check in

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxController : MonoBehaviour {
+
+    public GameObject loot;
+
+	// Use this for initialization
+	void Start () {
+
+        loot.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+
+	void OnCollisionStay2D(Collision2D coll)
+	{
+		
+		if (coll.gameObject.tag == "Player")
+		{
+            
+			    if (Input.GetKey(KeyCode.E))
+			    {
+                    if(loot.tag == "Loot")
+                    {
+                        loot.SetActive(true);
+                    }
+
+			    }
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D player_rb;
+	private float nextJump;
+
+	// Use this for initialization
+	void Start () {
+		player_rb = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 movement = new Vector2(Input.GetAxis("Horizontal") * speed,0f);
+
+		if (Time.time >= nextJump + 1)
+		{	
+			//player_rb.AddForce(new Vector2(0f, Input.GetAxis("Vertical") * jumpforce));
+			this.transform.Translate(new Vector2(0f, Input.GetAxis("Vertical") * 2 ));
+			nextJump = Time.time;
+		}
+
+
+		this.transform.Translate(movement);
+	}
+
+}
